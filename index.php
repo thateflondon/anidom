a784ccde9158742e465be071167cc59dcc68f0d9
Jerry Frederic <jerry.fpl@gmail.com>
2020-05-19 04:47:23 +0200

    header terminé

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>AniDom</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/reset.css">
 
 </head>
+
 <body>
     <div class="menu">
         <div class="main-nav">
@@ -20,10 +23,18 @@
             </ul>
         </div>
         <div class="sub-nav">
-
+            <ul>
+                <li><a href="#">Liste des Animaux</a></li>
+                <li><a href="#">Génériques</a></li>
+                <li><a href="#">Propriétaires</a></li>
+                <li><a href="#">Ajouter un Animal</a></li>
+                <li><a href="#">Gestion des Animaux</a></li>
+                <li><a href="#">Liste Animaux</a></li>
+            </ul>
         </div>
     </div>
     <div></div>
-    
+
 </body>
+
 </html>
